91a72f4da45c52acf76cf402985c557883594e78
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-09-10 10:04:16 +0200

    small upgrade at the buttons and nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,35 +13,47 @@ const skills = [
 ];
 
 const projects = [
-    {src: './assets/img/projects/fortfolio.png', description: 'Fortfolio: is a web page for a webDev deploy you own projects, based on HBS, CSS, JavaScript for frontend, in the backend using Nodejs, Express, MongoDB as a database.', gitRepo:'https://github.com/TahaRafaLinoOrg/project2', deployLink:'https://project-portfolio.adaptable.app/'},
-    {src: './assets/img/projects/botiga_cafe.png', description: 'Coffee shop: make a page for a coffee shop with HTML, CSS, and responsive', gitRepo:'https://github.com/Rgerotto/Botiga_de_cafes', deployLink:'https://botigadicoffee.netlify.app'},
-    {src: './assets/img/projects/projectEstagiario.png', description: 'WebPage: project to improve CSS and mobile version.', gitRepo: 'https://github.com/Rgerotto/projeto-Estagiario', deployLink:'https://internishp-project.netlify.app'},
-    {src: './assets/img/projects/Nomadnest_Voyages.png', description: 'Nomadnest: project from a corse from Udemy to be Master in CSS exploring animation, flexbox and transition', gitRepo:'https://github.com/Rgerotto/Natours', deployLink:'https://nomadnest-voyages.netlify.app'},
-    {src: './assets/img/projects/trillo.png', description: 'Trillo: project from Udemy course CSS Master to inprove my CSS skills, improve more my animation, transiction and flexbox', gitRepo: 'https://github.com/Rgerotto/Trillo', deployLink: 'https://trillo-rafael.netlify.app/'},
+    {src: './assets/img/projects/fortfolio.avif', description: 'Fortfolio: is a web page for a webDev deploy you own projects, based on HBS, CSS, JavaScript for frontend, in the backend using Nodejs, Express, MongoDB as a database.', gitRepo:'https://github.com/TahaRafaLinoOrg/project2', deployLink:'https://project-portfolio.adaptable.app/'},
+    {src: './assets/img/projects/botiga_cafe.avif', description: 'Coffee shop: make a page for a coffee shop with HTML, CSS, and responsive', gitRepo:'https://github.com/Rgerotto/Botiga_de_cafes', deployLink:'https://botigadicoffee.netlify.app'},
+    {src: './assets/img/projects/projectEstagiario.avif', description: 'WebPage: project to improve CSS and mobile version.', gitRepo: 'https://github.com/Rgerotto/projeto-Estagiario', deployLink:'https://internishp-project.netlify.app'},
+    {src: './assets/img/projects/Nomadnest_Voyages.avif', description: 'Nomadnest: project from a corse from Udemy to be Master in CSS exploring animation, flexbox and transition', gitRepo:'https://github.com/Rgerotto/Natours', deployLink:'https://nomadnest-voyages.netlify.app'},
+    {src: './assets/img/projects/trillo.avif', description: 'Trillo: project from Udemy course CSS Master to inprove my CSS skills, improve more my animation, transiction and flexbox', gitRepo: 'https://github.com/Rgerotto/Trillo', deployLink: 'https://trillo-rafael.netlify.app/'},
     /* {src: './assets/img/projects/trillo.jng', description: 'Nexter: project from Udemy CSS Master to improve CSS grid', gitRepo: 'https://github.com/Rgerotto/nexter', deployLink: 'https://github.com/Rgerotto/nexter'}, */
-    {src: './assets/img/projects/black-jack.png', description: 'Game: Project from Udemy JavaScript to improve DOM  monipulation', gitRepo: 'https://github.com/Rgerotto/Black_jack', deployLink: 'https://whimsical-gnome-13d6be.netlify.app/'},
-    {src: './assets/img/projects/guess_my_number.png', description: 'Game: Project from Udemy JavaScript to improve DOM  monipulation', gitRepo: 'https://github.com/Rgerotto/Guess_my_number', deployLink: 'https://moonlit-axolotl-de3c90.netlify.app'},
+    {src: './assets/img/projects/black-jack.avif', description: 'Game: Project from Udemy JavaScript to improve DOM  monipulation', gitRepo: 'https://github.com/Rgerotto/Black_jack', deployLink: 'https://whimsical-gnome-13d6be.netlify.app/'},
+    {src: './assets/img/projects/guess_my_number.avif', description: 'Game: Project from Udemy JavaScript to improve DOM  monipulation', gitRepo: 'https://github.com/Rgerotto/Guess_my_number', deployLink: 'https://moonlit-axolotl-de3c90.netlify.app'},
     //{src: './assets/img/projects/work_in_progress.jpg', description: 'HelpFornos: Landing Page for a bussines made with HTML, CSS, JavaScript', gitRepo: 'https://', deployLink: 'https://'}
 ];
 
+/* this is off */
 const chk = document.getElementById('chk');
-
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
 })
 
+const navLinks = document.querySelectorAll('.nav-list a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(navLinks => {
+            navLinks.classList.remove('active')
+        })
+        link.classList.add('active')
+    })
+})
+
 function createCard() {
+
+    /* <div class="btn">
+            <button class="btn-repo"><a href="${project.gitRepo}" target="_blank"><i class="fa-brands fa-github"></i></a></button>
+            <button class="btn-repo"><a href="${project.deployLink}" target="_blank"><i class="fa-solid fa-rocket"></i></a></button>
+            </div>
+             */
     projects.forEach(project => {
         const container = document.getElementsByClassName('container-card')[0];
         const create = document.createElement('div');
         create.className = 'card';
         create.innerHTML = 
             `<img class="proj" src="${project.src}" alt="${project.description}">
-            <div class="btn">
-            <button class="btn-repo"><a href="${project.gitRepo}" target="_blank"><i class="fa-brands fa-github"></i></a></button>
-            <button class="btn-repo"><a href="${project.deployLink}" target="_blank"><i class="fa-solid fa-rocket"></i></a></button>
-            </div>
-            
             `;
         container.appendChild(create);
 
@@ -72,6 +84,7 @@ function showPopupSkill(imagePathSkill, descriptionSkillText) {
     const popupImgSkill = document.getElementById('img-popup');
     const descriptionElement = document.getElementById('description-popup');
 
+
     if (popupSkill && popupImgSkill && descriptionElement) {
         // Set the image source and description
         popupImgSkill.src = imagePathSkill;
@@ -94,12 +107,33 @@ function showPopupSkill(imagePathSkill, descriptionSkillText) {
 function showPopup(imagePath, description, gitRepo, deployLink) {
     // Get the popup element and its contents
     const popup = document.querySelector('.popup');
-    const popupImage = document.getElementById('popup-image');
-    const popupDescription = document.getElementById('popup-description');
+    const popupContent = document.querySelector('.popup-content')
+    const popupImage = document.querySelector('#popup-image');
+    const popupDescription = document.querySelector('#popup-description');
+    const divBtn = document.querySelector('.div-btn');
 
+    
     // Set the image source and description
     popupImage.src = imagePath;
     popupDescription.textContent = description;
+
+    divBtn.innerHTML = '';
+
+    const btnGit = document.createElement('button');
+    btnGit.classList.add('btn-repo')
+    btnGit.innerHTML = '<i class="fa-brands fa-github"></i>';
+    btnGit.addEventListener('click', () => {
+        window.open(gitRepo, '_blank')
+    })
+    divBtn.appendChild(btnGit)
+
+    const btnDeploy = document.createElement('button');
+    btnDeploy.classList.add('btn-repo')
+    btnDeploy.innerHTML = '<i class="fa-solid fa-rocket"></i>';
+    btnDeploy.addEventListener('click', () => {
+        window.open(deployLink, '_blank')
+    })
+    divBtn.appendChild(btnDeploy)
 
     // Show the popup
     popup.classList.remove('hidden');
